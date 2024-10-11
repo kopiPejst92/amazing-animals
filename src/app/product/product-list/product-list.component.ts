faf91bef9ace8d758011a371a3ce6e7fcdd50377
kopiPejst92 <agkop01@gmail.com>
2024-10-11 15:13:54 +0200

    filtering added

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CartService } from 'src/app/cart/cart.service';
 import { Product } from 'src/app/models/product';
 import { ProductService } from '../product.service';
-import { CartService } from 'src/app/cart/cart.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -12,6 +12,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class ProductListComponent implements OnInit {
 
   products: Product[] = []
+  filteredProducts: Product[] = []
+  sortOrder: string = ""
 
   constructor(private productService: ProductService, private cartService: CartService, private snackbar: MatSnackBar) {
 
@@ -20,15 +22,38 @@ export class ProductListComponent implements OnInit {
   ngOnInit(): void {
     this.productService.getProducts().subscribe(data => {
       this.products = data;
+      this.filteredProducts = data;
     })
   }
 
   addToCart(product: Product): void {
     this.cartService.addToCart(product).subscribe({
       next: () => {
-        this.snackbar.open("Added to cart")
+        this.snackbar.open("Added to cart", "", {
+          duration: 2000,
+          horizontalPosition: 'right',
+          verticalPosition: 'top'
+        })
       }
     });
   }
 
+  applyFilter(event: Event) {
+    let searchTerm = (event?.target as HTMLInputElement).value;
+    searchTerm = searchTerm.toLowerCase();
+
+    this.filteredProducts = this.products.filter(product => product.name.toLowerCase().includes(searchTerm)
+)
+    this.sortProducts(this.sortOrder)
+  }
+
+  sortProducts(sortValue: string) {
+    this.sortOrder = sortValue;
+
+    if (this.sortOrder === "priceLowHigh") {
+      this.filteredProducts.sort((a, b) => a.price - b.price)
+    } else if (this.sortOrder === "priceHighLow") {
+      this.filteredProducts.sort((a, b) => b.price - a.price)
+    }
+  }
 }
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -2,7 +2,9 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { ProductListComponent } from './product-list/product-list.component';
-import {MatSnackBarModule} from '@angular/material/snack-bar'; 
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select'
 
 @NgModule({
   declarations: [
@@ -11,7 +13,9 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
   imports: [
     CommonModule,
     MatCardModule, 
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatInputModule,
+    MatSelectModule
   ]
 })
 
